d4901a0c99edb5f866254b36dfa7bffa70a2e106
Fabio Bustamante Romillo <f.bustamanteromillo@gmail.com>
2025-10-19 11:40:36 -0400

    Cloned repo and added Native Unit Test Project

---
+++ RectangleTests/RectangleTests.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" {
+#include "../BCSRec/main.h"
+}
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RectangleTests
+{
+	TEST_CLASS(RectangleTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
